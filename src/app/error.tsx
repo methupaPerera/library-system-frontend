0985f51559ede49fadd91a563f4d4e4e0af51976
methupaPerera <methupapereradev@gmail.com>
2024-03-29 18:20:06 +0530

    Minor changes.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Error({ reset }: { reset: () => void }) {
     return (
         <div className="mx-auto h-[calc(100vh-3.5rem)] flex flex-col gap-2 items-center justify-center font-bold">
-            <h2 className="text-foreground text-3xl">Something went wrong !</h2>
+            <h2 className="text-foreground text-3xl">Error. Something went wrong !</h2>
 
             <button className="underline" onClick={() => reset()}>
                 Try again.
